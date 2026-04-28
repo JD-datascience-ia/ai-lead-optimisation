38d21a112efb3e2457be0dfaf8d72b3a4390e807
Julien Deboté <julien.debote34170@gmail.com>
2026-04-28 13:53:20 +0200

    Premier essai du main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from src.preprocessing import load_data
 from src.scoring import rule_score
+from src.ml_model import train_model
 import pandas as pd
 
-df = load_data("data/simulated_leads_nova_lead.csv")
+path = "data/simulated_leads_nova_lead.csv"
 
-df = df.read_csv("data/simulated_leads_nova_lead.csv")
+df = load_data(path)
 
-df["score"] = df.apply(rule_score, axis=1)
+#Scoring
 
-print(df.sort_values("score", ascending=False).head(10))
+df["rule_score"] = df.apply(rule_score, axis=1)
+
+#Model
+
+model, features = train_model(path)
+
+X = pd.get_dummies(df[["source", "intent"]])
+X["response_delay"] = df["response_delay_min"]
+
+X = X.reindex(columns=features, fill_value=0)
+
+df["ml_score"] = model.predict_proba(X)[:, 1] * 100
+
+print(df[["rule_score", "ml_score", "converted"]].head(10))
