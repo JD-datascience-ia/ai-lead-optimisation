8a817ffd17df5f719c8a4662e788012c7c3cfd40
Julien Deboté <julien.debote34170@gmail.com>
2026-04-28 13:36:45 +0200

    Debut du main

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from src.rule_scoring import rule_score
+from src.preprocessing import load_data
+from src.scoring import rule_score
 import pandas as pd
+
+df = load_data("data/simulated_leads_nova_lead.csv")
 
 df = df.read_csv("data/simulated_leads_nova_lead.csv")
 
 df["score"] = df.apply(rule_score, axis=1)
+
+print(df.sort_values("score", ascending=False).head(10))
